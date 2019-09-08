1745b77fde62ddcc1dc9189b56b53f4ea1d9982f
David S <david.schriemer7@gmail.com>
2019-09-08 14:24:20 -0600

    Initial settup

---
+++ Concentrate/Concentrate/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Concentrate
+//
+//  Created by David Schriemer on 2019-09-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    }
+
+
+}
+
